eca1e0c054d0f293c67bf9df008dcfc0fbe8d3e5
Dustin Bates <dustin.bates98@gmail.com>
2023-03-22 14:37:56 -0600

    Joe is a genius

--- allSpice.client/src/env.js
+++ allSpice.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'codeworks-classroom.us.auth0.com'
+export const clientId = 'vag5QXyI2zuu0wjCwXSUUFPg0mzGVTNV'
+export const audience = 'https://codeworks-classroom.com'
